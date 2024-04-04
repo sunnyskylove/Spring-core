643bc079997b532717df0c4a150f02e69af0d841
YS <sunnyskylove22@gmail.com>
2024-04-04 17:14:48 +0900

    설명 추가

--- 01_lecture-source/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
+++ 01_lecture-source/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -8,13 +8,17 @@ public class Application {
 
     public static void main(String[] args) {
 
+        // 스프링 컨텍스트를 생성하고, XML 파일에서 빈(bean) 구성을 로드한다.
+        // 이때, XML 파일의 경로를 인자로 전달한다.
         ApplicationContext context =
                 new GenericXmlApplicationContext("section01/xmlconfig/spring-context.xml");
 
+        // 스프링 컨텍스트에서 MemberDTO 타입의 빈을 가져와 member 변수에 할당한다.
         MemberDTO member = context.getBean(MemberDTO.class);
 
-//        System.out.println("member = " + member);
+//        System.out.println("member = " + member);    확인
 
+        // 실행할 요소
         System.out.println(member.getPersonalAccount());
         System.out.println(member.getPersonalAccount().deposit(10000));     // 만원 입금!
         System.out.println(member.getPersonalAccount().getBalance());             // 잔액 확인
